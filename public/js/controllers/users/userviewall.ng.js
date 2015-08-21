1fab3552c4b2003f0918663a6c09432724e67656
vimes1984 <churchill.c.j@gmail.com>
2015-08-21 20:02:00 +0200

    adding in roles to viewall page

--- public/js/controllers/users/userviewall.ng.js
+++ public/js/controllers/users/userviewall.ng.js
@@ -2,5 +2,5 @@
 angular.module('adminui').controller('UserViewAllCtrl', ['$scope', '$meteor', function($scope, $meteor){
 
   $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
-
+  console.log($scope.users);
 }]);
