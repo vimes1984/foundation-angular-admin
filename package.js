b1fff49294bad601cd02c22525b8392cfb790e16
vimes1984 <churchill.c.j@gmail.com>
2015-08-22 18:40:45 +0200

    adding in new version number 0.1 YEAH!

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /*Global*/
 Package.describe({
   name: 'vimes1984:foundation-angular-admin',
-  version: '0.0.18',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Installs an admin ui using zurb',
   // URL to the Git repository containing the source code for this package.
