1abf44b9f4eabf2e935039458fbf0a31da02134a
growlf <growlf@oddotter.com>
2015-08-24 15:55:57 -0700

    Fixing admin page accesss

--- public/js/routes.ng.js
+++ public/js/routes.ng.js
@@ -1,61 +1,64 @@
 user = Meteor.user();
-if (Roles.userIsInRole(user, 'super-admin')) {
+//if (Roles.userIsInRole(user, 'super-admin')) {
 
 
-    angular.module('adminui').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
-        function ($urlRouterProvider, $stateProvider, $locationProvider) {
+angular.module('adminui').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+    function ($urlRouterProvider, $stateProvider, $locationProvider) {
 
 
-            $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
 
-            $stateProvider
-                .state('admin', {
-                    url: '/admin',
-                    templateUrl: 'vimes1984_foundation-angular-admin_client/templates/admin.ng.html',
-                    controller: 'MainCtrl',
-                    resolve: {
-                        "currentUser": ["$meteor", function ($meteor) {
-                            return $meteor.requireUser();
-                        }]
-                    },
-                })
-                .state('admin.dashboard', {
-                    url: '/dashboard',
-                    templateUrl: 'vimes1984_foundation-angular-admin_client/templates/admindashboard.ng.html',
-                    controller: 'MainCtrl'
-                })
-                .state('admin.viewallusers', {
-                    url: '/viewallusers',
-                    templateUrl: 'vimes1984_foundation-angular-admin_client/templates/adminallusers.ng.html',
-                    controller: 'UserViewAllCtrl'
-                })
-                .state('admin.editusers', {
-                    url: '/edituser/:userID',
-                    templateUrl: 'vimes1984_foundation-angular-admin_client/templates/adminedituser.ng.html',
-                    controller: 'AdminEditUserCtrl'
-                })
-                .state('admin.addnewuser', {
-                    url: '/addnewuser',
-                    templateUrl: 'vimes1984_foundation-angular-admin_client/templates/adminnewuser.ng.html',
-                    controller: 'AdminAddNewUserCtrl'
-                })
-                .state('admin.addnewpage', {
-                    url: '/addnewpage',
-                    templateUrl: 'vimes1984_foundation-angular-admin_client/templates/addnewpage.ng.html',
-                    controller: 'AdminAddNewPageCtrl'
-                })
-                .state('admin.viewpages', {
-                    url: '/viewallpages',
-                    templateUrl: 'vimes1984_foundation-angular-admin_client/templates/viewallpages.ng.html',
-                    controller: 'AdminViewAllPagesCtrl'
-                })
-                .state('admin.editpage', {
-                    url: '/editpage/:pageID',
-                    templateUrl: 'vimes1984_foundation-angular-admin_client/templates/admineditpage.ng.html',
-                    controller: 'AdminEditPageCtrl'
-                });
-        }]);
-}
+        $stateProvider
+            .state('admin', {
+                url: '/admin',
+                templateUrl: 'vimes1984_foundation-angular-admin_client/templates/admin.ng.html',
+                controller: 'MainCtrl',
+                resolve: {
+                    "currentUser": ["$meteor", function ($meteor) {
+                        user = Meteor.user();
+                        if (Roles.userIsInRole(user, ['admin'])) {
+                            return user;
+                        }
+                    }]
+                }
+            })
+            .state('admin.dashboard', {
+                url: '/dashboard',
+                templateUrl: 'vimes1984_foundation-angular-admin_client/templates/admindashboard.ng.html',
+                controller: 'MainCtrl'
+            })
+            .state('admin.viewallusers', {
+                url: '/viewallusers',
+                templateUrl: 'vimes1984_foundation-angular-admin_client/templates/adminallusers.ng.html',
+                controller: 'UserViewAllCtrl'
+            })
+            .state('admin.editusers', {
+                url: '/edituser/:userID',
+                templateUrl: 'vimes1984_foundation-angular-admin_client/templates/adminedituser.ng.html',
+                controller: 'AdminEditUserCtrl'
+            })
+            .state('admin.addnewuser', {
+                url: '/addnewuser',
+                templateUrl: 'vimes1984_foundation-angular-admin_client/templates/adminnewuser.ng.html',
+                controller: 'AdminAddNewUserCtrl'
+            })
+            .state('admin.addnewpage', {
+                url: '/addnewpage',
+                templateUrl: 'vimes1984_foundation-angular-admin_client/templates/addnewpage.ng.html',
+                controller: 'AdminAddNewPageCtrl'
+            })
+            .state('admin.viewpages', {
+                url: '/viewallpages',
+                templateUrl: 'vimes1984_foundation-angular-admin_client/templates/viewallpages.ng.html',
+                controller: 'AdminViewAllPagesCtrl'
+            })
+            .state('admin.editpage', {
+                url: '/editpage/:pageID',
+                templateUrl: 'vimes1984_foundation-angular-admin_client/templates/admineditpage.ng.html',
+                controller: 'AdminEditPageCtrl'
+            });
+    }]);
+//}
 
 
 angular.module('adminui').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
