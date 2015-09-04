05311e9ebfbf3ff899fd225a0f4280b49dc6569c
vimes1984 <churchill.c.j@gmail.com>
2015-09-04 23:01:46 +0200

    adding in logo for front end

--- public/js/appinit.ng.js
+++ public/js/appinit.ng.js
@@ -10,3 +10,10 @@ Template.registerHelper('site',
 function(){
   return Site.find({}).fetch()[0];
 });
+Template.registerHelper('logo',
+function(){
+  var imageid = Site.find({}).fetch()[0];
+  var logo = Images.find({_id: imageid.sitelogo}).fetch()[0];
+  console.log(logo.copies.images.name);
+  return logo.copies.images.name;
+});
