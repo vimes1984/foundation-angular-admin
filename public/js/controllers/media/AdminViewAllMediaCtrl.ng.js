4634558ea749f90585a69afd55be9adf29752ec6
vimes1984 <churchill.c.j@gmail.com>
2015-09-14 00:27:36 +0200

    adding in array

--- public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
+++ public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
@@ -1,8 +1,13 @@
 angular.module('adminui').controller('AdminViewAllMediaCtrl', ['$scope', '$meteor', '$state', function($scope, $meteor, $state){
 
-  $scope.media = MediaCollection.find({}).fetch();
+  $scope.mediascope = MediaCollection.find({}).fetch();
   $scope.mediaurl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
   $scope.thumburl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
 
-  console.log($scope.media)
+  angular.forEach($scope.mediascope, function(value, key){
+
+    var thisfile        = Media.find({_id: value.media}).fetch()[0];
+    value.thismedia = thisfile;
+  });
+    console.log($scope.mediascope);
 }]);
