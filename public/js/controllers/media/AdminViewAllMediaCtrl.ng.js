8ba347428bdab5f1ea0ed1a276319402e0758732
growlf <growlf@oddotter.com>
2015-09-13 15:15:16 -0700

    Adding thumbnails

--- public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
+++ public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
@@ -2,7 +2,7 @@ angular.module('adminui').controller('AdminViewAllMediaCtrl', ['$scope', '$meteo
 
   $scope.media = MediaCollection.find({}).fetch();
   $scope.mediaurl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
-  //$scope.thumburl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
+  $scope.thumburl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
 
   console.log($scope.media)
 }]);
