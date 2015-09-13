923da004e6536bc5ac4b056fb769516cb1999467
growlf <growlf@oddotter.com>
2015-09-13 15:02:36 -0700

    changed Images to Media

--- public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
+++ public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
@@ -1,7 +1,8 @@
 angular.module('adminui').controller('AdminViewAllMediaCtrl', ['$scope', '$meteor', '$state', function($scope, $meteor, $state){
 
   $scope.media = MediaCollection.find({}).fetch();
-  $scope.uploadurl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
+  $scope.mediaurl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
+  //$scope.thumburl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
 
   console.log($scope.media)
 }]);
