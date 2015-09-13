39e26dc1adf0f44e856c20b343b8e2706fbf084e
growlf <growlf@oddotter.com>
2015-09-13 15:43:29 -0700

    Reformating

--- public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
+++ public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
@@ -1,13 +1,13 @@
-angular.module('adminui').controller('AdminViewAllMediaCtrl', ['$scope', '$meteor', '$state', function($scope, $meteor, $state){
+angular.module('adminui').controller('AdminViewAllMediaCtrl', ['$scope', '$meteor', '$state', function ($scope, $meteor, $state) {
 
-  $scope.mediascope = MediaCollection.find({}).fetch();
-  $scope.mediaurl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
-  $scope.thumburl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
+    $scope.mediascope = MediaCollection.find({}).fetch();
+    $scope.mediaurl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
+    $scope.thumburl = FS.HTTP.uploadUrl + "/" + Media.find().fetch()[0].collectionName;
 
-  angular.forEach($scope.mediascope, function(value, key){
+    angular.forEach($scope.mediascope, function (value, key) {
 
-    var thisfile        = Media.find({_id: value.media}).fetch()[0];
-    value.thismedia = thisfile;
-  });
+        var thisfile = Media.find({_id: value.media}).fetch()[0];
+        value.thismedia = thisfile;
+    });
     console.log($scope.mediascope);
 }]);
