26557f5c20b6e60bcc5c1f30627d16319a4f8e6c
growlf <growlf@oddotter.com>
2015-09-13 16:31:04 -0700

    reformating

--- public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
+++ public/js/controllers/media/AdminViewAllMediaCtrl.ng.js
@@ -1,31 +1,31 @@
 angular.module('adminui').controller('AdminViewAllMediaCtrl', ['$scope', '$meteor', '$state', function ($scope, $meteor, $state) {
 
-  $scope.mediascope = $meteor.collection(MediaCollection);
+    $scope.mediascope = $meteor.collection(MediaCollection);
 
     angular.forEach($scope.mediascope, function (value, key) {
-    var thisfile        = Media.findOne({_id: value.media});
-    var originalopts         = {
-      store: 'media',
-      auth: null,
-      download: false,
-      metadata: false,
-      brokenIsFine: false,
-      uploading: null, // return this URL while uploading
-      storing: null, // return this URL while storing
-      filename: null
-    };
-    var thumbopts         = {
-      store: 'thumbs',
-      auth: null,
-      download: false,
-      metadata: false,
-      brokenIsFine: false,
-      uploading: null, // return this URL while uploading
-      storing: null, // return this URL while storing
-      filename: null
-    };
-    value.original     = thisfile.url(originalopts);
-    value.thumbnail     = thisfile.url(thumbopts);
-  });
+        var thisfile = Media.findOne({_id: value.media});
+        var originalopts = {
+            store: 'media',
+            auth: null,
+            download: false,
+            metadata: false,
+            brokenIsFine: false,
+            uploading: null, // return this URL while uploading
+            storing: null, // return this URL while storing
+            filename: null
+        };
+        var thumbopts = {
+            store: 'thumbs',
+            auth: null,
+            download: false,
+            metadata: false,
+            brokenIsFine: false,
+            uploading: null, // return this URL while uploading
+            storing: null, // return this URL while storing
+            filename: null
+        };
+        value.original = thisfile.url(originalopts);
+        value.thumbnail = thisfile.url(thumbopts);
+    });
     console.log($scope.mediascope);
 }]);
